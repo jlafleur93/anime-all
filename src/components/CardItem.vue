416eef21d072b7d2aa8f077fd4f249f32eb24007
joelwowz <josephlafleur@hotmail.com>
2021-08-28 22:25:17 -0600

    more layout changes

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -18,11 +18,17 @@
 
 <style lang="scss">
   .card-margin {
-    display: grid;
-    grid-template-rows: 2fr 1fr;
+    min-height: 320px;
     width: 31.333%;
     background-color: #a20021;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     border-radius: 9px;
+  }
+  .card-area {
+    width: 100%;
+    padding: 0;
+    display: grid;
+    grid-auto-rows: auto;
+    justify-self: center;
   }
 </style>
